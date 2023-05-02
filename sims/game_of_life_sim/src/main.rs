e3b939268416ec1465f3c1ec7b0dd2f392d106b5
Jacob LeCoq <lecoqjacob@gmail.com>
2023-05-01 19:00:59 -0500

    part3
    fix window's workflows

--- sims/game_of_life_sim/src/main.rs
+++ sims/game_of_life_sim/src/main.rs
@@ -2,8 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
 use game_of_life_sim::ShaderPlaygroundPlugin;
+use std::io::Cursor;
+use winit::window::Icon;
 
 fn main() {
     App::new()
@@ -18,5 +22,25 @@ fn main() {
             ..default()
         }))
         .add_plugin(ShaderPlaygroundPlugin)
+        .add_startup_system(set_window_icon)
         .run();
+}
+
+// Sets the icon on windows and X11
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window: Query<Entity, With<PrimaryWindow>>,
+) {
+    let primary_entity = primary_window.single();
+    let primary = windows.get_window(primary_entity).unwrap();
+    let icon_buf = Cursor::new(include_bytes!(
+        "../build/macos/AppIcon.iconset/icon_256x256.png"
+    ));
+    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+        let image = image.into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary.set_window_icon(Some(icon));
+    };
 }
